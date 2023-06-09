584b51f88f34f7e72b782f45d94f4494d93e6047
TianLongMengXue <e3e4e20@126.com>
2023-06-09 16:09:23 +0800

    指定前端启动端口号,启动时自动开启浏览器

--- fontend/vue.config.js
+++ fontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        port: 80,
-        open: true,
+        port: 80, // npm run serve 启动时的端口号
+        open: true, // npm run serve 自动启动浏览
     }
 });
