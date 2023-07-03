6a4afeb019f8f47f4fae102fa682eefb3a9ed16b
TianLongMengXue <e3e4e20@126.com>
2023-07-03 17:29:20 +0800

    禁用项目启动时浏览自动启动

--- fontend/vue.config.js
+++ fontend/vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
         port: 80, // npm run serve 启动时的端口号
-        open: true, // npm run serve 自动启动浏览
+        open: false, // npm run serve 自动启动浏览
     }
 });
