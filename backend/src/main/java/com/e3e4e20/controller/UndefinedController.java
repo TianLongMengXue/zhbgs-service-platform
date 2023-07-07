3a7abb6efb43746870c2a13b43345d7a41924313
TianLongMengXue <e3e4e20@126.com>
2023-07-07 15:09:52 +0800

    重构404页面

--- backend/src/main/java/com/e3e4e20/controller/UndefinedController.java
+++ backend/src/main/java/com/e3e4e20/controller/UndefinedController.java
@@ -3,8 +3,11 @@ package com.e3e4e20.controller;
 import com.e3e4e20.utils.ResponseData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -15,9 +18,10 @@ Author: 天龙梦雪
 @CrossOrigin
 @RestController
 @Api(tags = {"404页面管理"}, value = "未定义页面内容控制器")
-public class HomeController {
-    @RequestMapping("/404")
-    public ResponseData index () {
+public class UndefinedController {
+    private final Logger log = LoggerFactory.getLogger("Class: AvatarController ");
+    @RequestMapping(value = "/404")
+    public ResponseData post404 () {
         return ResponseData.NOT_FOUND();
     }
 }
